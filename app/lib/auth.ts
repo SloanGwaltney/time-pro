20bfc516341c065c678811372588e86dbee52400
SloanGwaltney <sloangwaltney2@gmail.com>
2022-07-09 17:14:54 -0400

    added require User Id function

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -6,11 +6,20 @@ import { commitSession, sessionHasRole } from "~/session";
 export async function authorizeOrRedirect(session: Session, role: UserRole, attemptedAction: string) {
 	const authorized = isAuthorized(session, role)
 	if (!authorized) {
-		session.set('error', `You do not have permission to ${attemptedAction}`)
+		session.flash('error', `You do not have permission to ${attemptedAction}`)
 		throw redirect('/', {headers: {'Set-Cookie': await commitSession(session)}})
 	}
 }
 
 function isAuthorized(session: Session, role: UserRole) {
 	return sessionHasRole(session, role)
+}
+
+export function requireUserId(session: Session) {
+	const userId = session.get('userId')
+	if (!userId) {
+		session.flash('error', 'You must be logged in to perform this action')
+		throw redirect('/', {status: 401})
+	}
+	return userId
 }
