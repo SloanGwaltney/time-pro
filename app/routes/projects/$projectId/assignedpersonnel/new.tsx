eea815f89ed56e21be68d1f7b6d462cfcf90bc39
SloanGwaltney <86498828+SloanGwaltney@users.noreply.github.com>
2022-07-13 17:32:25 -0400

    Personnel Assignment (#3)
    * Can now assign employees to projects
    
    * removed console.logs

---
+++ app/routes/projects/$projectId/assignedpersonnel/new.tsx
@@ -0,0 +1,52 @@
+import { UserRole } from '@prisma/client'
+import type { ActionFunction, LoaderFunction} from '@remix-run/node';
+import { redirect} from '@remix-run/node';
+import { json } from '@remix-run/node'
+import {Form as RemixForm, useTransition} from '@remix-run/react'
+import { prisma } from 'prisma/prisma.server'
+import { Button, Form } from 'react-bulma-components'
+import { z } from 'zod'
+import { authorizeOrRedirect } from '~/lib/auth'
+import { parseFormData, validateBodyOrReturnResponse } from '~/lib/form'
+import { getSession } from '~/session'
+
+const assignPersonnelSchema = z.object({
+	email: z.string().email()
+})
+
+export const loader: LoaderFunction = async ({request}) => {
+	const session = await getSession(request.headers.get('Cookie'))
+	await authorizeOrRedirect(session, UserRole.PROJECT_MANAGER, 'Assign Personnel to a Project')
+	return null
+}
+
+export const action: ActionFunction = async ({request, params}) => {
+	const session = await getSession(request.headers.get('Cookie'))
+	await authorizeOrRedirect(session, UserRole.PROJECT_MANAGER, 'Assign Personnel to a Project')
+	const body = await parseFormData(request)
+	const validationResult = await validateBodyOrReturnResponse(body, assignPersonnelSchema)
+	const user = await prisma.user.findUnique({where: {email: validationResult.data.email}, select: {id: true}})
+	if (!user) return json({validationError: {email: 'Invalid Email'}})
+	await prisma.projectPersonnel.create({data: {assignedUserId: user.id, projectId: Number(params.projectId)}})
+	return redirect(`/projects/${params.projectId}`)
+}
+
+const NewProjectPersonnel: React.FC<{}> = () => {
+	const transition = useTransition()
+
+	return (
+		<div>
+			<RemixForm method='post'>
+				<Form.Field>
+					<Form.Label>Email</Form.Label>
+					<Form.Control>
+						<Form.Input name="email" />
+					</Form.Control>
+				</Form.Field>
+				<Button disabled={transition.state === 'submitting'} fullwidth rounded color="primary" type='submit'>{transition.state === 'submitting' ? 'Submitting' : 'Assign Personnel'}</Button>
+			</RemixForm>
+		</div>
+	)
+}
+
+export default NewProjectPersonnel
